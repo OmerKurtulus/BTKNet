ac8b5d172ce029057548f95318eb3b9bab92795f
Ömer Kurtuluş <omerkurtulus@Omer-MacBook-Pro.local>
2022-08-15 15:01:50 +0300

    Person has been defined

--- index.js
+++ index.js
@@ -3,15 +3,29 @@ var i, sum = 0;
 let a = "Dev-OmerKurtulus"
 
 document.getElementById("root").innerHTML = 'Hellodev';
+const arr = [
+  {
+  id:1,
+  name: "Sude Akgün",
+  age:19
+  },
+  {
+    id:2,
+    name: "Omer Kurtulus",
+    age:21
+    },
+    {
+      id:3,
+      name: "Betul Samanci",
+      age:21
+      },
+]
 
-  for(i = 0; i <= 100; i++) {
+  for(let person of arr) {
   const element = document.createElement("div");
-  element.innerHTML = i;
+  element.innerHTML = `${person.id} ${person.name} ${person.age}`;
   document.getElementById("root").append(element);
-  console.log(i);
-  
-
-  }
+}
 
  
 
