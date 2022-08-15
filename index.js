63a6828e597763dcdfa4479f54f45a811a90085f
Ömer Kurtuluş <omerkurtulus@Omer-MacBook-Pro.local>
2022-08-15 13:52:09 +0300

    This is my first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("hello")
+var i, sum = 0;
+let a = "Dev-OmerKurtulus"
+
+document.getElementById("root").innerHTML = 'Hellodev';
+
+  for(i = 0; i <= 100; i++) {
+  const element = document.createElement("div");
+  element.innerHTML = i;
+  document.getElementById("root").append(element);
+  console.log(i);
+  
+
+  }
+
+ 
+
